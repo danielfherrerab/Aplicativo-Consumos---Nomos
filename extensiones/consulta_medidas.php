deab1b715ef37add3edbc0e6e058bcca8db882c2
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-13 22:19:42 +0000

    Consulta y css

--- extensiones/consulta_medidas.php
+++ extensiones/consulta_medidas.php
@@ -29,7 +29,7 @@
 		$total = 0;
 		if(mysqli_num_rows($medidas) >= 1){
 			while($refe = mysqli_fetch_array($medidas)){
-				$consumos = mysqli_query($conexion, "SELECT sum(cantidad) as cantidad,medida from consumo_planchas as consumo inner join articulos on articulos.id_articulo = consumo.id_articulo where medida like '%$refe[medida]%' and  (fecha_consumo between '$solo_fecha' and '$fecha_final') GROUP BY id_consumo");
+				$consumos = mysqli_query($conexion, "SELECT sum(cantidad) as cantidad,medida from consumo_planchas as consumo inner join articulos on articulos.id_articulo = consumo.id_articulo where medida like '%$refe[medida]%' and  (fecha_consumo between '$solo_fecha' and '$fecha_final') GROUP BY medida");
 				while($recorrer = mysqli_fetch_array($consumos)){
 					echo "<tr><td>$recorrer[medida]</td><td>$recorrer[cantidad]</td></td></tr>";
 					$total = $total + $recorrer["cantidad"];
