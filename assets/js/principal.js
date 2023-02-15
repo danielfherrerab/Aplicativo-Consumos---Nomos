cd0843b4c5786feb2fe82fd2f9e48ede4a9dec85
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-15 19:45:43 +0000

    Arreglo

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -168,10 +168,9 @@ function traerinfo(id){
 
 /* PARA TRAER INFO ORDEN */
 function traerOrden(op) {
-	op = op.replace(/\bop\b/g, "");console.log(op+orden);
-	var orden = document.getElementById("op").value;
+	op = op.replace(/\bop\b/g, "");console.log(op);
 	
 	var nombre_trabajo = document.getElementById("trabajo"+op).value;
 	$('textarea[name=nombre_trabajo').val(nombre_trabajo);
-	$('input[name=busqueda_OP').val(orden);
+	$('input[name=busqueda_OP').val(op);
 }
